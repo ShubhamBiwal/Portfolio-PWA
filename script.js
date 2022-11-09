4710a5816e297d8d7e001a0de7400e48874bb6a8
Shubham Kumar <100457482+ShubhamBiwal@users.noreply.github.com>
2022-11-09 20:02:48 +0530

    initial-commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let menu = document.querySelector('#menu-bar');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  header.classList.remove('active');
+}
+
+// typing Animation
+ var typed = new Typed(".typing",{
+  strings: ["Web Developer","front-end developer","PHP Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+ });
+
+//  register
+if("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+    .register("/serviceWorker.js")
+    .then(res => console.log("service worker registered"))
+    .catch(err => console.log("service worker not registered", err));
+  });
+}
